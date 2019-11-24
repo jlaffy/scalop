f0447f0434e3e40deb5261b3d04b49f1490c9e2b
JULIE LAFFY <julie.laffy@weizmann.ac.il>
2019-11-24 13:32:57 +0200

    update ordering function

--- R/jaccard.R
+++ R/jaccard.R
@@ -7,10 +7,13 @@
 #' @description This function computes pairwise Jaccard Similarities for all pairs of character vectors provided. Flexibility on input allows for three use cases: (1) In the simplest use case, two character vectors are provided to x and y arguments. (2) Alternatively, a single list of character vectors can be passed to x, in which case Jaccard similarities will be computed for all pairs of vectors in the list. (3) Lastly, two (different) lists of character vectors are provided to x and y arguments, respectively, in which case Jacccard similarities will be computed for all inter-list pairs of vectors.
 #' @param x character vector or list of character vectors
 #' @param y optional character vector or list of character vectors. If x is a character vector, y must be provided. Default: NULL
+#' @param order reorder rows and columns by hierarchical clustering. Not applicable if <x> and <y> are character vectors. Default: F
+#' @param dist.method distance metric to be computed, applicable if <order> is TRUE. See scalop::dist.methods. Default: "manhattan"
+#' @param cluster.method the agglomeration method to be used in hierachical clustering. See scalop::cluster.methods. Default: "average"
 #' @return numeric Jaccard value between x and y if both are character vectors or a matrix of Jaccard values between all pairs of character vectors amongst x if x is a list and y was not provided, or between x and y if y was provided. If x and y were provided, the elements in x correspond to the columns in the output matrix.
 #' @rdname jaccard
 #' @export 
-jaccard <- function(x, y = NULL, order = F, dist.method = 'manhattan') {
+jaccard <- function(x, y = NULL, order = F, dist.method = 'manhattan', cluster.method = 'average') {
     if (!is.null(dim(x))) {
         x = as.list(as.data.frame(x, stringsAsFactors = F))
     }
@@ -22,7 +25,8 @@ jaccard <- function(x, y = NULL, order = F, dist.method = 'manhattan') {
     are.lists = sum(sapply(list(x, y), is.list)) == 2
     stopifnot(are.chars|are.lists)
     if (are.chars) return(.jaccard(x = x, y = y))
+
     jacmat = comply(x, y, FUN = .jaccard)
-    mat = reorder(jacmat, col = order, row = order)
+    mat = hca_reorder(x = jacmat, cor.method = NULL, dist.method = dist.method, cluster.method = cluster.method, col = order, row = order)
     mat
 }
