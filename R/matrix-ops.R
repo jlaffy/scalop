7ab77ff87ba3df4fb47eeb7c486851b421f19d16
JULIE LAFFY <julie.laffy@weizmann.ac.il>
2019-11-24 13:33:41 +0200

    minor fix argument name

--- R/matrix-ops.R
+++ R/matrix-ops.R
@@ -60,7 +60,7 @@ rowdetected = function(m) {
 
 #' @title <dim> for many matrices
 #' @description Returns the result of dim for every matrix in a list
-#' @param m a list of matrices (or a single matrix)
+#' @param mats a list of matrices (or a single matrix)
 #' @return dim for each matrix provided.
 #' @rdname dims
 #' @export 
@@ -76,7 +76,7 @@ dims <- function(mats) {
 
 #' @title <ncol> for many matrices
 #' @description Returns the result of ncol for every matrix in a list
-#' @param m a list of matrices (or a single matrix)
+#' @param mats a list of matrices (or a single matrix)
 #' @return ncol for each matrix provided.
 #' @rdname ncols
 #' @export 
@@ -92,7 +92,7 @@ ncols <- function(mats) {
 
 #' @title <nrow> for many matrices
 #' @description Returns the result of nrow for every matrix in a list
-#' @param m a list of matrices (or a single matrix)
+#' @param mats a list of matrices (or a single matrix)
 #' @return nrow for each matrix provided.
 #' @rdname nrows
 #' @export 
