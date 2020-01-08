2c8ca3cacf0277f0b5290dcad9570098dff1dc52
Julie Laffy <julie.laffy@weizmann.ac.il>
2020-01-08 19:43:34 +0200

    jaccard ++

--- R/comply.R
+++ R/comply.R
@@ -1,15 +1,15 @@
 
-#' @title Apply a Function to All Combinations in One or between a Pair of Vectors
-#' @description Apply a Function to All Combinations in One or between a Pair of Vectors
+#' @title Apply a Function to All Pairs of Elements in 'x' or Between 'x' and 'y' 
+#' @description Apply a Function to All Pairs of Elements in 'x' or Between 'x' and 'y' 
 #' @param x a vector or a list of vectors
 #' @param y a vector or a list of vectors. If y is NULL, y will be set to x, such that if x is a list, the function FUN will be applied to all pairs of vectors in the list. Default: NULL
-#' @param FUN the function to apply to all pairs of x and y. Default: jaccard
+#' @param FUN the function to be applied to every pair of elements in 'x', or to every pair of elements between 'x' and 'y'. The function should take two arguments and return a numeric value. Default: jaccard
+#' @param ... optional arguments to 'FUN'.
+#' @param simplify logical; should the result be simplified to a matrix? Default: TRUE
 #' @return a matrix of resulting values after applying FUN to all pairs of x and y (or the one pair if x and y are both atomic vectors). The number of columns and the number of rows will correspond to the lengths of x and y respectively.
-#' @seealso 
-#'  \code{\link[stats]{setNames}}
 #' @rdname comply
 #' @export 
-comply = function(x, y = NULL, FUN = jaccard) {
+comply = function(x, y = NULL, FUN = jaccard, ..., simplify = TRUE) {
     # map function FUN to all combinations of x and y
     # if y is null, y is set to x
 
@@ -27,10 +27,18 @@ comply = function(x, y = NULL, FUN = jaccard) {
     colm = names(x)
     m = outer(seq_along(x),
               seq_along(y),
-              FUN = Vectorize(function(i,j) FUN(x[[i]], y[[j]])))
-    m = t(m)
-    rownames(m) = rows
-    colnames(m) = colm
+              FUN = Vectorize(function(i,j) FUN(x[[i]], y[[j]], ...)))
+
+    if (is.matrix(m)) {
+        m = t(m)
+        rownames(m) = rows
+        colnames(m) = colm
+    }
+
+    if (!simplify && is.matrix(m)) {
+        m = as.list(as.data.frame(m))
+    }
+
     m
 } 
 
--- R/jaccard.R
+++ R/jaccard.R
@@ -7,31 +7,29 @@
 #' @description This function computes pairwise Jaccard Similarities for all pairs of character vectors provided. Flexibility on input allows for three use cases: (1) In the simplest use case, two character vectors are provided to x and y arguments. (2) Alternatively, a single list of character vectors can be passed to x, in which case Jaccard similarities will be computed for all pairs of vectors in the list. (3) Lastly, two (different) lists of character vectors are provided to x and y arguments, respectively, in which case Jacccard similarities will be computed for all inter-list pairs of vectors.
 #' @param x character vector or list of character vectors
 #' @param y optional character vector or list of character vectors. If x is a character vector, y must be provided. Default: NULL
-#' @param order reorder rows and columns by hierarchical clustering. Not applicable if <x> and <y> are character vectors. Default: F
-#' @param dist.method distance metric to be computed, applicable if <order> is TRUE. See scalop::dist.methods. Default: "manhattan"
-#' @param cluster.method the agglomeration method to be used in hierachical clustering. See scalop::cluster.methods. Default: "average"
 #' @return numeric Jaccard value between x and y if both are character vectors or a mrix of Jaccard values between all pairs of character vectors amongst x if x is a list and y was not provided, or between x and y if y was provided. If x and y were provided, the elements in x correspond to the columns in the output mrix.
 #' @rdname jaccard
 #' @export 
-jaccard <- function(x, y = NULL, order = F, col.order = order, row.order = order, dist.method = 'manhattan', cluster.method = 'average') {
+jaccard <- function(x, y = NULL, simplify = TRUE) {
     if (!is.null(dim(x))) {
         x = as.list(as.data.frame(x, stringsAsFactors = F))
     }
-    if (is.null(y)) y = x
+    
+    if (is.null(y)) {
+        y = x
+    }
+
     if (!is.null(dim(y))) {
         y = as.list(as.data.frame(y, stringsAsFactors = F))
     }
+
     are.chars = sum(sapply(list(x, y), is.character)) == 2
     are.lists = sum(sapply(list(x, y), is.list)) == 2
     stopifnot(are.chars|are.lists)
-    if (are.chars) return(.jaccard(x = x, y = y))
 
-    jacmat = comply(x, y, FUN = .jaccard)
-    m = hca_reorder(x = jacmat,
-                    cor.method = 'none',
-                    dist.method = dist.method,
-                    cluster.method = cluster.method,
-                    col = col.order,
-                    row = row.order)
-    m
+    if (are.chars) {
+        return(.jaccard(x = x, y = y))
+    }
+
+    comply(x, y, FUN = .jaccard)
 }
