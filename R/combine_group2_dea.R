5bd96d90abd69204349e0831ecb18e09e264eb73
Julie Laffy <julie.laffy@weizmann.ac.il>
2020-01-09 19:14:57 +0200

    doc

--- R/combine_group2_dea.R
+++ R/combine_group2_dea.R
@@ -1,4 +1,8 @@
 
+#' @title Aggregate `scalop::dea` output for one group against many in turn
+#' @param L.dea list of gene-named-numeric log2-fold change values
+#' @param coverage prioritise genes that appear as DE in at least this fraction of the dea result vectors in 'L.dea'. Default: 0.5
+#' @param return.avg.lfc logical; if FALSE (default) return gene names only (ordered by average log2 fold change). If TRUE, return average values. Default: FALSE
 #' @rdname combine_group2_dea
 #' @export
 combine_group2_dea = function(L.dea, coverage = 0.5, return.avg.lfc = FALSE) {
--- R/jacmap.R
+++ R/jacmap.R
@@ -1,4 +1,24 @@
 
+#' @title Convert a list to a jaccard matrix and plot with `scalop::gmap`
+#' @param L list of character vectors to compute jaccard indexes between. 
+#' @param ratio aspect ratio. Default: 1
+#' @param limits colour key limits. Default: c(0, 1)
+#' @param tile.col draw lines between tiles. Default: 'black'
+#' @param tile.size draw lines between tiles. Default: 0.1
+#' @param cols colours for key. Default: c("white", "darkred")
+#' @param num numeric (TRUE) or character (FALSE) x- and y- axes labels. Default: FALSE
+#' @param angle angle x-axis labels. Default: T
+#' @param ... other arguments passed to `scalop::gmap` 
+#' @param tiletext write jaccard indexes on tiles? Default: FALSE
+#' @param dcp relevant if tiletext is TRUE. How many decimal places to show? Default: 1
+#' @return ggplot object 
+#' @seealso 
+#'  \code{\link[reshape2]{melt}}
+#'  \code{\link[ggplot2]{geom_label}}
+#' @rdname jacmap
+#' @export 
+#' @importFrom reshape2 melt
+#' @importFrom ggplot2 geom_text
 jacmap = function(L,
                   ratio = 1,
                   limits = c(0, 1),
