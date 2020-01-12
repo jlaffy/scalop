e03ee5f72d23945af7947f72efc372f6af1a897d
Julie Laffy <julie.laffy@weizmann.ac.il>
2020-01-12 10:18:28 +0200

    nSigGenes, combine group2

--- R/nSigGenes.R
+++ R/nSigGenes.R
@@ -1,4 +1,24 @@
 
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param group PARAM_DESCRIPTION, Default: NULL
+#' @param m PARAM_DESCRIPTION, Default: NULL
+#' @param p PARAM_DESCRIPTION, Default: c(0.01, 0.001, 1e-04)
+#' @param center.rows PARAM_DESCRIPTION, Default: TRUE
+#' @param lfc PARAM_DESCRIPTION, Default: log2(2L)
+#' @param pmethod PARAM_DESCRIPTION, Default: 'BH'
+#' @param alternative PARAM_DESCRIPTION, Default: 'greater'
+#' @param simplify PARAM_DESCRIPTION, Default: TRUE
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname nSigGenes
+#' @export 
 nSigGenes = function(group = NULL,
                      m = NULL,
                      p = c(0.01, 1e-03, 1e-04),
