7f1f53877f406b0e9b990c7c0b56b1c9f63950a2
Julie Laffy <julie.laffy@weizmann.ac.il>
2020-01-17 18:16:41 +0200

    ggdensity with bimodal fit, legend dimensions in gmap

--- R/add_to_dataframe.R
+++ R/add_to_dataframe.R
@@ -3,7 +3,7 @@
 #' @description FUNCTION_DESCRIPTION
 #' @param dat dataframe
 #' @param vec named vector whose names are a column of the dataframe
-#' @param col the name of the column that joining should be performed on.
+#' @param by the name of the column that joining should be performed on.
 #' @param newcol the name of the new column to add from vec. Default: 'val'
 #' @return dataframe with the same number of rows as 'dat' and an additional column corresponding to the values of 'vec'.
 #' @seealso 
@@ -11,8 +11,8 @@
 #' @rdname add_to_dataframe
 #' @export 
 #' @importFrom dplyr left_join
-add_to_dataframe = function(dat, vec, col, newcol = 'val') {
+add_to_dataframe = function(dat, vec, by, newcol = 'val') {
     dat.vec = data.frame(join=names(vec), val=vec, stringsAsFactors = F)
-    colnames(dat.vec) = c(col, newcol)
+    colnames(dat.vec) = c(by, newcol)
     dplyr::left_join(dat, dat.vec)
 }
--- R/ggdensity.R
+++ R/ggdensity.R
@@ -1,14 +1,91 @@
 
-ggBimodalDensity = function(data, x, fit.bimodal = FALSE, ...) {
-    if (fit.bimodal) {
-        vals = stats::setNames(data[[x]], rownames(data))
-        modes = fitBimodal(vals, assign = T, ...)
-        if (length(modes) == 1) return(ggpubr::ggdensity(data, x = x))
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param data PARAM_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @param xlab PARAM_DESCRIPTION, Default: 'Cell Score'
+#' @param mode.names PARAM_DESCRIPTION, Default: c("Mode1", "Mode2")
+#' @param ... PARAM_DESCRIPTION
+#' @param prob PARAM_DESCRIPTION, Default: 0.95
+#' @param coverage PARAM_DESCRIPTION, Default: 0.95
+#' @param min.size PARAM_DESCRIPTION, Default: 10
+#' @param na.action PARAM_DESCRIPTION, Default: c("show", "hide", "combine")
+#' @param combine.with PARAM_DESCRIPTION, Default: 1
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso 
+#'  \code{\link[stats]{setNames}}
+#'  \code{\link[ggpubr]{ggdensity}}
+#'  \code{\link[scalop]{Unlist}},\code{\link[scalop]{add_to_dataframe}},\code{\link[scalop]{flip}}
+#'  \code{\link[dplyr]{filter}},\code{\link[dplyr]{mutate}}
+#' @rdname ggDensity
+#' @export 
+#' @importFrom stats setNames
+#' @importFrom ggpubr ggdensity
+#' @importFrom scalop Unlist add_to_dataframe flip
+#' @importFrom dplyr filter mutate
+ggDensity = function(data,
+                     x,
+                     xlab = 'Cell Score',
+                     mode.names = c('Mode1', 'Mode2'),
+                     
+                     ...,
+                     prob = 0.95,
+                     coverage = 0.95,
+                     min.size = 10,
+                     na.action = c('show','hide','combine'),
+                     combine.with = 1) {
+
+    na.action = match.arg(na.action)
+    vals = stats::setNames(data[[x]], data[[1]])
+    modes = fitBimodal(vals,
+                       assign = T,
+                       coverage = coverage,
+                       prob = prob,
+                       size = min.size)
+
+    if (length(modes) == 1) {
+        return(ggpubr::ggdensity(data, x = x, ...))
     }
 
-    names(modes) <- c('Mode1', 'Mode2')
-    modes = stats::setNames(rep(names(modes), lengths(modes)),unlist(modes))
-    data$Mode = modes[rownames(data)]
-    palette =c("#00AFBB", "#E7B800", "#FC4E07")
-    ggpubr::ggdensity(data, x = x, col = 'Mode', fill = 'Mode', rug = T, add = 'mean')
+    names(modes) <- mode.names
+    nas <- names(vals)[!names(vals) %in% unlist(modes)]
+    modes = c(modes, list(`NA`=nas))
+    modes0 = modes
+    modes = scalop::Unlist(modes)
+    data = scalop::add_to_dataframe(data,
+                                    vec = scalop::flip(modes),
+                                    col = colnames(data)[1],
+                                    newcol = 'Mode')
+    #palette =c("#00AFBB", "#E7B800", "#FC4E07")
+    if (na.action == 'hide') {
+        data = data %>%
+            dplyr::filter(Mode != 'NA')
+    }
+
+    else if (na.action == 'combine') {
+        data = data %>%
+            dplyr::mutate(Mode = ifelse(Mode == 'NA',
+                                        mode.names[combine.with],
+                                        as.character(Mode)))
+    }
+
+    G <- ggpubr::ggdensity(data,
+                           x = x,
+                           xlab = xlab,
+                           col = 'Mode',
+                           ylab = 'Density',
+                           fill = 'Mode',
+                           rug = T,
+                           add = 'mean',
+                           ...)
+
+    plot(G)
+    return(invisible(list(data = data, modes = modes0)))
 }
