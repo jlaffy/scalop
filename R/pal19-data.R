79e07d13d1f5ceea3839f589b09f960ad3181ac4
Julie Laffy <julie.laffy@weizmann.ac.il>
2020-05-12 15:24:00 +0300

    palette and QC

---
+++ R/pal19-data.R
@@ -0,0 +1,14 @@
+#' pal19, 19 Pretty Discrete Colours
+#'
+#' @docType data
+#'
+#' @usage data(pal19)
+#'
+#' @format A character vector.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(pal19)
+#' scales::show_col(pal19)
+"pal19"
