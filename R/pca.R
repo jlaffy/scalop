d762fb53f3c221aa7509f45d0581cda4e5119561
Julie Laffy <julie.laffy@weizmann.ac.il>
2020-12-08 12:37:44 +0200

    fixed itemize in documentation return value

--- R/pca.R
+++ R/pca.R
@@ -7,10 +7,12 @@
 #' @param center A logical value indicating whether the variables should be shifted to be zero centered. Alternately, a centering vector of length equal to the number of columns of 'm' can be supplied. Default: TRUE
 #' @param scale A logical value indicating whether the variables should be scaled to have unit variance before the analysis is performed. The value of 'scale' determines how column scaling is performed (after centering). If 'scale' is a numeric vector with length equal to the number of columns of 'm', then each column of 'm' is divided by the corresponding value from 'scale'. If 'scale' is 'TRUE', then scaling is done by dividing the (centered) columns of 'm' by their standard deviations if 'center=TRUE', and the root mean square otherwise.  If 'scale' is 'FALSE', no scaling is done. Default: FALSE
 #' @return A list of class "prcomp" containing the following components:
-#' * sdev, the standard deviations of the principal components (i.e. the square roots of the eigenvalues of the covariance/correlation matrix, though the calculation is actually done with the singular values of the data matrix).
-#' * rotation, the matrix of variable loadings (i.e. a matrix whose columns contain the eigenvectors).
-#' * x, if 'retx' is 'TRUE' the value of the rotated data (the centered (and scaled if requested) data multipleied by the 'rotation' matrix) is returned. Hence, 'cov(m)' is the diagonal matrix 'diag(sdev^2)'.
-#' * center, scale, the centering and scaling used, or 'FALSE'.
+#' \itemize{
+#' \item {sdev:} {the standard deviations of the principal components (i.e. the square roots of the eigenvalues of the covariance/correlation matrix, though the calculation is actually done with the singular values of the data matrix).}
+#' \item {rotation:} {the matrix of variable loadings (i.e. a matrix whose columns contain the eigenvectors).}
+#' \item {x:} {if 'retx' is 'TRUE' the value of the rotated data (the centered (and scaled if requested) data multipleied by the 'rotation' matrix) is returned. Hence, 'cov(m)' is the diagonal matrix 'diag(sdev^2)'.}
+#' \item {center, scale:} {the centering and scaling used, or 'FALSE'.}
+#' }
 #' @details see 'irlba::prcomp_irlba()' for more details. 
 #' @seealso 
 #'  \code{\link[irlba]{prcomp_irlba}}
