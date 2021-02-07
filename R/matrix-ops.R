7bf595e6c119ed0ecc992d899145ca46afed2d17
jlaffy <julie.laffy@weizmann.ac.il>
2021-02-07 13:29:16 +0200

    matrixStats::rowMeans2/matrixStats::colMeans2 instead of default

--- R/matrix-ops.R
+++ R/matrix-ops.R
@@ -7,8 +7,8 @@
 #' @export 
 colcenter = function(m, by = 'mean') {
     m = as.matrix(m)
-    if (by == 'mean')  by = matrixStats::colMeans2(m)
-    else if (by == 'median') by = matrixStats::colMedians(m)
+    if (by == 'mean')  by = matrixStats::colMeans2(m, na.rm = T)
+    else if (by == 'median') by = matrixStats::colMedians(m, na.rm = T)
     else stopifnot(is.numeric(by) & length(by) == ncol(m))
     scale(m, center = by, scale = F)
 }
@@ -22,8 +22,8 @@ colcenter = function(m, by = 'mean') {
 #' @export 
 rowcenter = function(m, by = 'mean') {
     m = as.matrix(m)
-    if (by == 'mean')  by = matrixStats::rowMeans2(m)
-    else if (by == 'median') by = matrixStats::rowMedians(m)
+    if (by == 'mean')  by = matrixStats::rowMeans2(m, na.rm = T)
+    else if (by == 'median') by = matrixStats::rowMedians(m, na.rm = T)
     else stopifnot(is.numeric(by) & length(by) == nrow(m))
     t(scale(t(m), center = by, scale = F))
 }
