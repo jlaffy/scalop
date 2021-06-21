5cc2f2e3b08b3525ba02abe663ef7bce34f333ac
Julie Laffy <julie.laffy@weizmann.ac.il>
2021-06-21 10:59:13 +0300

    ggmap, ggraster

--- R/ggmap.R
+++ R/ggmap.R
@@ -1,4 +1,21 @@
 
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param m PARAM_DESCRIPTION
+#' @param ... PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso 
+#'  \code{\link[reshape2]{melt}}
+#' @rdname ggmap
+#' @export 
+#' @importFrom reshape2 melt
 ggmap = function(m, ...) {
     gmap(reshape2::melt(as.matrix(m)), ...) 
 }
--- R/ggraster.R
+++ R/ggraster.R
@@ -1,4 +1,21 @@
 
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param m PARAM_DESCRIPTION
+#' @param ... PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso 
+#'  \code{\link[reshape2]{melt}}
+#' @rdname ggraster
+#' @export 
+#' @importFrom reshape2 melt
 ggraster = function(m, ...) {
     graster(reshape2::melt(as.matrix(m)), ...) 
 }
