e4b40d230e51f7c003f9a86df76e23c56bd9f386
Julie Laffy <julie.laffy@weizmann.ac.il>
2023-09-28 17:13:19 +0300

    Adding PanCancer signatures

---
+++ R/Signatures_Gavish2023.R
@@ -0,0 +1,8 @@
+#' Hallmarks of transcriptional intratumour heterogeneity across a thousand tumours
+#'
+#' Gavish, A., Tyler, M., Greenwald, A.C. et al. Hallmarks of transcriptional intratumour heterogeneity across a thousand tumours. Nature 618, 598–606 (2023). https://doi.org/10.1038/s41586-023-06130-4
+#'
+#' @docType data
+#'
+#' @format list of character vectors
+"Signatures_Gavish2023"
