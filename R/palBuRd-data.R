741ec7ed4fc55891e0c56a927988c16d695c4364
Julie Laffy <julie.laffy@weizmann.ac.il>
2024-03-19 17:23:09 +0200

    heatmap colour palette

---
+++ R/palBuRd-data.R
@@ -0,0 +1,11 @@
+#' Divergent Heatmap Colours from Blue to Red
+#'
+#' @docType data
+#'
+#' @format A character vector of 11 heatmap colours from blue to red, with the middle value as whote
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' scales::show_col(palBuRd)
+"palBuRd"
