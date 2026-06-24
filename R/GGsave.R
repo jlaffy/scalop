036c1314148c577f9d46c62a2dbfcd83d3de1058
Julie Laffy <12819478+jlaffy@users.noreply.github.com>
2026-06-24 14:24:20 -0400

    Update Imports (drop Rcpp; add Homo.sapiens, limma, tidyr); tidy formatting

--- R/GGsave.R
+++ R/GGsave.R
@@ -1,13 +1,12 @@
 GGsave = function(filename, 
-		  plot=last_plot(), 
-		  width=dev.size()[1],
-		  height=dev.size()[2]) {
-
-    device = 'png'
-    if (str_detect(filename, "pdf")) device = 'pdf'
-    ggsave(filename=filename,
-	   plot=plot,
-	   width=width,
-	   height=height,
-	   device=device)
+                  plot=last_plot(), 
+		              width=dev.size()[1],
+		              height=dev.size()[2]) {
+  device = 'png'
+  if (str_detect(filename, "pdf")) device = 'pdf'
+  ggsave(filename=filename,
+         plot=plot,
+	       width=width,
+	       height=height,
+	       device=device)
 }
